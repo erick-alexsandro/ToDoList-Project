f18301b5b76f758bb7aa867ffac2f687aadf7376
Erick Alexsandro <erickalexsandrocontato@gmail.com>
2023-11-15 15:34:16 -0300

    Added 'new', 'list' and 'delete' functions.

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 A Simple ToDo List to keep track of tasks.
 
 # How to Use It
+
+| Commands  | Result |
+| ------------- | ------------- |
+| new  | Add A Task  |
+| list  | List All Tasks  |
+| delete | Remove Specific Task |
+| quit (or q) | Quit App |
+
+---
+
+`The app must be used with the browser's console opened.`
--- todos.js
+++ todos.js
@@ -1,8 +1,27 @@
 let input = prompt('What would you like to do?');
-while (input !== 'quit') {
-    while (input !== "q") {
-        input = prompt('What would you like to do?')
+const todos = ['(This is one of the tasks)'];
+
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        console.log('--------------------- \n \n');
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`)
+        }
+        console.log('\n ---------------------');
+    } else if (input === 'new') {
+        let newTask = prompt('Add new task:');
+        todos.push(newTask);
+        console.log(`New task added: ${newTask}`)
+    } else if (input === 'delete') {
+        const index = parseInt(prompt('Which task are you going to delete?'))
+        if (!Number.isNaN(index)) {
+            console.log(`You deleted '${index}: ${todos[index]}' from the list.`)
+            const deleted = todos.splice(index, 1)
+        } else {
+            console.log('Invalid input.')
+        }
     }
+    input = prompt('What would you like to do?')
 }
 
 console.log('You have quit the app.')
