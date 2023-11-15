b38a030e17e8da3003024243a0047fa58a3ab786
Erick Alexsandro <erickalexsandrocontato@gmail.com>
2023-11-15 12:27:12 -0300

    Iinitial Commit

---
+++ todos.js
@@ -0,0 +1,8 @@
+let input = prompt('What would you like to do?');
+while (input !== 'quit') {
+    while (input !== "q") {
+        input = prompt('What would you like to do?')
+    }
+}
+
+console.log('You have quit the app.')
